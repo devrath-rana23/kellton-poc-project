d4adc03d194a7f429e2464a7c19d5fac93470e2d
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-17 18:30:38 +0530

    loader done

--- src/components/common/Loader/Loader.js
+++ src/components/common/Loader/Loader.js
@@ -3,30 +3,13 @@ import "./Loader.css";
 
 export const Loader = () => {
   return (
-    <div className="loader-container">
-      <div className="loader-div">
-        <svg
-          className="loader-svg"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="loader-opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="loader-opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
+    <>
+      <div id="myElem1">
+        <div class="cv-spinner">
+          <span class="spinner"></span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
