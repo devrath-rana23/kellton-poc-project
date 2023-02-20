ccb04efced4097ffd78585953cd391b22be91d7e
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-20 14:30:56 +0530

    fix

--- src/components/layout/Unauthorized/Unauthorized.js
+++ src/components/layout/Unauthorized/Unauthorized.js
@@ -7,16 +7,16 @@ export const Unauthorized = (props) => {
     <div className="authBody">
       <section className="leftSection"></section>
       <section className="rightSection">
+        <section className="logoSection">
+          <figure className="logoContainer">
+            <img className="logo" src={ImageUrls.logo} alt="logo" />
+          </figure>
+        </section>
         <section className="signInSection">
           <div className="poc-heading-container">
             <h1 className="poc-heading">PROJECT NAME</h1>
           </div>
           {props.children ?? <Outlet />}
-        </section>
-        <section className="logoSection">
-          <figure className="logoContainer">
-            <img className="logo" src={ImageUrls.logo} alt="logo" />
-          </figure>
         </section>
       </section>
     </div>
